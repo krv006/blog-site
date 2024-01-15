4864006ff9ff53eef924cb83b89d51a30c55190d
Пользователь <rvkamronbek@gmail.com>
2024-01-15 16:23:40 +0500

    blog_site

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -3,12 +3,18 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    
+    
     path('', index),
     path('about/', about, name="about"),
+    
     
     path('blogs/', all_blog, name="all_blog"),#
     path('blog/<int:blog_id>', blog_detail , name="blog_detail"), #
     
-    path('work/<int:work_id>', work , name="work"), 
-
+    
+    path('work/<int:work_id>', work_detail , name="work_detail"),  
+    path('work/', work, name="work"),
+   
+    
 ]
